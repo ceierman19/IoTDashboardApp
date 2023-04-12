51d28f7a62318bad20a2806bdc62f44d6f5d763c
ceierman19 <ceierman19@bw.edu>
2023-04-11 20:42:44 -0400

    Attempted to add web services

--- app/src/main/java/com/example/iotdashboardapp/AllSensorsFragment.java
+++ app/src/main/java/com/example/iotdashboardapp/AllSensorsFragment.java
@@ -2,9 +2,17 @@ package com.example.iotdashboardapp;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.iotdashboardapp.model.AuthRequest;
+import org.json.JSONObject;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -47,6 +55,8 @@ public class AllSensorsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //ServiceClient.sharedServiceClient(getApplicationContext());
+        //ServiceClient.sharedServiceClient(null);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -58,6 +68,31 @@ public class AllSensorsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_all_sensors, container, false);
+
+        view.findViewById(R.id.buttonGetAllSensorData).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View getAllSensorDataButton) {
+                AuthRequest.username = "ceierman19";
+                AuthRequest.password = "changeme";
+                JsonObjectRequest request = new AuthRequest(Request.Method.GET, "https://mopsdev.bw.edu/~ceierman19/csc330/architecture_template/www/rest.php/orders", null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        TextView sensorDataField = view.findViewById(R.id.textViewAllSensorData);
+                        sensorDataField.setText(response.toString());
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Volley Error", error.toString());
+                        //TODO: handle error
+                    }
+                });
+                //ServiceClient client = ServiceClient.sharedServiceClient(getApplicationContext());
+                //ServiceClient client = ServiceClient.sharedServiceClient(null);
+                //client.addRequest(request);
+            }
+        });
+
         return view;
     }
 }
